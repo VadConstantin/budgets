1bdb41e1fc18932d3e1ae372957f2e76e8e9f803
Vadim Constantin <vad.constantin@gmail.com>
2021-12-15 22:17:08 +0100

    javascript to prevent edit form without comment or amount

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -20,7 +20,7 @@ class PaymentsController < ApplicationController
 
   def update
 
-    # on efface la dette du payeur et l'incrementation du total du budget
+    # EFFACE PAYMENT - on efface la dette du payeur et l'incrementation du total du budget
 
     @budget = Budget.find(params[:budget_id])
     @payment = Payment.find(params[:id])
@@ -47,11 +47,55 @@ class PaymentsController < ApplicationController
     @budget.save
     @payment.destroy
 
-    # on met à jour la dette du payeur et on incremente le total du budget
+    # RECREE PAYMENT - on met à jour la dette du payeur et on incremente le total du budget
 
-    return create
+    @budget = Budget.find(params[:budget_id])
+    params[:payment][:commentaire].capitalize!
+
+    @payment = Payment.new(payment_params)
+    @payment.budget_id = @budget.id
+    if @payment.save
+
+      # ------------------PAYEUR---------------------
+
+      @payeur = params[:payment][:payeurs] #=> "1"
+      u = UserPayment.create(payment_id: @payment.id, user_id: @payeur.to_i, state: "payeur")
+      u.save!
+
+      # ----------------RECEVEUR(S)------------------
+
+      @receveurs = params[:payment][:receveurs][1...10] #=> ["1", "3"]
+      @receveurs.each do |receveur|
+        v = UserPayment.create(payment_id: @payment.id, user_id: receveur.to_i, state: "receveur")
+        v.save!
+      end
+
+      # ------------------BALANCE--------------------
+      @user_budget_payeur = UserBudget.where(budget_id: @budget.id).where(user_id: @payeur.to_i)
+      if @receveurs.length == 1 && @receveurs[0] == @payeur
+        @user_budget_payeur[0].dette += 0
+      elsif @receveurs.include?(@payeur)
+        @user_budget_payeur[0].dette += (params[:payment][:montant_cents]).to_f.fdiv(@receveurs.length)
+        @user_budget_payeur[0].save
+      elsif @receveurs.include?(@payeur) == false
+        @user_budget_payeur[0].dette += params[:payment][:montant_cents].to_i
+        @user_budget_payeur[0].save
+      end
+
+    else
+      render :new
+    end
+
+     # ------------------TOTAL----------------------
+    @budget.total_cents += params[:payment][:montant_cents].to_i
+    @budget.save
+
+    if @payment.save
+      redirect_to budget_path(@budget)
+    end
 
   end
+
 
   def create
 
--- app/javascript/controllers/formeditpayment_controller.js
+++ app/javascript/controllers/formeditpayment_controller.js
@@ -4,24 +4,37 @@ export default class extends Controller {
   static targets = ["checkbox", "checkboxAll", "button"]
 
   connect() {
-    console.log("hello formeditpayment")
-    console.log(document.querySelectorAll('input:checked').length)
-    console.log(this.buttonTarget)
+    console.log("hello formeditpayment");
+    console.log(document.getElementById("payment_montant_cents").value);
+
     if (document.querySelectorAll('input:checked').length < 2) {
       this.buttonTarget.firstElementChild.disabled = true
     }
+
   }
 
   checker() {
-    // if (document.querySelectorAll('input:checked').length > 2) {
-    //   this.checkboxTarget.checked = false
-    // }
 
+  // si              LONGUEUR < 2            ET             CHAMP VIDE
     if (document.querySelectorAll('input:checked').length < 2) {
       this.buttonTarget.firstElementChild.disabled = true
     } else {
       this.buttonTarget.firstElementChild.disabled = false
     }
   }
+
+  remplir() {
+
+    const isEmpty = str => !str.trim().length;
+
+    if (isEmpty(document.getElementById("payment_montant_cents").value) || isEmpty(document.getElementById("payment_commentaire").value)) {
+        this.buttonTarget.firstElementChild.disabled = true
+      } else {
+        this.buttonTarget.firstElementChild.disabled = false;
+      }
+  }
+
+
+
 
 }
