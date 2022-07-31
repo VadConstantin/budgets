10ae310b13373611c0c42a3ee1699ef7ed5bc2fc
Vadim Constantin <vad.constantin@gmail.com>
2022-07-31 07:17:14 +0200

    improved deb status method with owe and owes cases

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -3,28 +3,28 @@ class Budget < ApplicationRecord
   has_many :users, through: :user_budgets
 
   has_many :payments, dependent: :destroy
-
   validates :name, presence: true
 
-
-  def get_debt_status
+  def debt_status(current_user)
     persons = []
-    UserBudget.where(budget_id: self.id).each do |n|
-        persons << {name: n.user.name, dette: n.dette}
-    end
-    if persons[1].nil? == false
+    UserBudget.where(budget_id: self.id).each { |n| persons << { name: n.user.name, dette: n.dette } }
+    if persons[1]
       if persons[0][:dette] > persons[1][:dette]
-        return "#{persons[1][:name]} owes #{(persons[0][:dette]) - (persons[1][:dette])} € to #{persons[0][:name]}"
+        return "#{persons[1][:name] == current_user.name ? "You owe" : "#{persons[1][:name]} owes"} #{(persons[0][:dette]) - (persons[1][:dette])} € to #{persons[0][:name] == current_user.name ? "You" : persons[0][:name]}"
       elsif persons[0][:dette] < persons[1][:dette]
-        return "#{persons[0][:name]} owes #{(persons[1][:dette]) - (persons[0][:dette])} € to #{persons[1][:name]}"
+        return "#{persons[0][:name] == current_user.name ? "You owe" : "#{persons[0][:name]} owes"} #{(persons[1][:dette]) - (persons[0][:dette])} € to #{persons[1][:name] == current_user.name ? "You" : persons[1][:name]}"
       else
         return "Nothing to pay"
       end
     end
   end
 
-  def get_participants
-
+  def other_participant(current_user)
+    participants = []
+    self.user_budgets.each do |n|
+      participants << n.user.name
+    end
+    return participants.filter { |user| user != current_user.name}
   end
 
 end
